ccc017a998c08fdd441f9348871b8e72375763d9
rutob42 <rutob42@gmail.com>
2024-07-05 09:22:43 +0300

    digital clock

---
+++ Abelton/project digital clock/script.js
@@ -0,0 +1,11 @@
+function UpdateClock(){
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2,'0');
+    const minutes = String(now.getMinutes()).padStart(2,'0');
+    const seconds = String(now.getSeconds()).padStart(2,'0');
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`
+}
+
+UpdateClock();
+setInterval(UpdateClock, 1000);
